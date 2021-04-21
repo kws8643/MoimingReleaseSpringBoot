90daa8a3cc98a923c03eae74ec72d3e4589964be
Peter Kang <peterkang@Peterui-MacBookPro.local>
2021-04-21 22:02:13 +0900

    Initial commit

---
+++ src/main/java/com/example/moiming_release/MoimingReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.moiming_release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimingReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoimingReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moiming_release/MoimingReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moiming_release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimingReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
